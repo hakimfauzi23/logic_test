d90f84ce3a4cc820abdcc6f5049a963bdcde0f44
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-01-19 20:32:07 +0700

    Logic Test Revision 1

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-echo "<pre><span class='inner-pre' style='font-size: 32px'>";
+echo "<pre><span class='inner-pre' style='font-size: 32px;'>";
 for ($row = 0; $row < 7; $row++) {
   for ($col = 0; $col <= 8; $col++) {
     if ($col == 1 or (($row == 0 or $row == 6) and ($col > 1 and $col < 8)) or ($col == 7 and $row != 0 and $row != 7)) {
-      echo "#";
+      echo " #";
     } elseif (($col == 2 and $row == 1) or ($col == 6 and $row == 1) or ($col == 2 and $row == 5) or ($col == 6 and $row == 5)) {
-      echo "#";
+      echo " #";
     } elseif (($col == 3 and $row == 2) or ($col == 5 and $row == 2) or ($col == 3 and $row == 4) or ($col == 5 and $row == 4)) {
-      echo "#";
+      echo " #";
     } elseif (($col == 4 and $row == 3)) {
-      echo "#";
+      echo " #";
     } else {
-      echo " ";
+      echo "  ";
     }
   }
   echo "<br>";
