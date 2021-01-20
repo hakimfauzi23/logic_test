bb6dc49b597177398b826228e8f97cee3e0c856c
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-01-20 21:37:19 +0700

    Revisi Logic Test 2

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-echo "<pre><span class='inner-pre' style='font-size: 32px;'>";
-for ($row = 0; $row < 7; $row++) {
-  for ($col = 0; $col <= 8; $col++) {
-    if ($col == 1 or (($row == 0 or $row == 6) and ($col > 1 and $col < 8)) or ($col == 7 and $row != 0 and $row != 7)) {
-      echo " #";
-    } elseif (($col == 2 and $row == 1) or ($col == 6 and $row == 1) or ($col == 2 and $row == 5) or ($col == 6 and $row == 5)) {
-      echo " #";
-    } elseif (($col == 3 and $row == 2) or ($col == 5 and $row == 2) or ($col == 3 and $row == 4) or ($col == 5 and $row == 4)) {
-      echo " #";
-    } elseif (($col == 4 and $row == 3)) {
-      echo " #";
-    } else {
-      echo "  ";
+$row_length = 7;
+$column_length = 7;
+$i = 0;
+for ($row = 1; $row <= $row_length; $row++) {
+    for ($column = 1; $column <= $column_length; $column++) {
+        if ($row == 1 || $row == ($row_length)) {
+            echo "#&nbsp;";
+        } else if ($column == 1 || $column == $column_length) {
+            echo "#&nbsp;";
+        } else if (($row == $row_length && $column == $column_length) || ($row == $row && $column == ($column_length - ($row - 1)))) {
+            echo "#&nbsp;";
+        } else if (($row == $row_length && $column == $column_length) || ($row == $row && $column == $row)) {
+            echo "#&nbsp;";
+        } else {
+            echo "&nbsp;&nbsp;&nbsp;";
+        }
     }
-  }
-  echo "<br>";
+    echo '<br>';
 }
-echo "</span></pre>";
